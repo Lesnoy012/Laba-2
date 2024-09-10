40cfc3867d7c457f0b7bb9168db45877e2b45e6f
Vlad Ipatov <royalevlad2@gmail.com>
2024-09-10 15:55:15 +0300

    v2

--- Laba #2/Form1.cs
+++ Laba #2/Form1.cs
@@ -13,26 +13,16 @@ namespace Laba__2
 {
     public partial class Form1 : Form
     {
+        const int n = 10000000;
+        int[] arr = new int[n];
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button_exit_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void button_left_Click(object sender, EventArgs e)
-        {
-            int key = (int)input_key_NeOptim.Value;
-
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            int n = 10000000;
-            int[] arr = new int[n];
 
             int StartTime = Environment.TickCount;
             {
@@ -42,6 +32,22 @@ namespace Laba__2
 
             int ResultTime = Environment.TickCount - StartTime;
             textBox_time_NeOptim.Text = ResultTime.ToString();
+        }
+        private void button_left_Click(object sender, EventArgs e)
+        {
+            int key = (int)input_key_NeOptim.Value;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == key)
+                {
+                    textBox_index_NeOptim.Text = i.ToString();
+                    break;
+                }
+            }
+        }
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
